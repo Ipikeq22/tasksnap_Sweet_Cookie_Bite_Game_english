2da2cc1cd14710dbfca4beec54c673fa7e0dcae5
Ipikeq22 <ipike388@gmail.com>
2025-12-03 02:59:38 +0800

    Delete start.js

--- start.js
+++
@@ -1,7 +0,0 @@
-function startGame() {
-    window.location.href = 'game.html';
-}
-
-function goHome() {
-    window.location.href = 'https://tasksnap-englishmode-b2kk6dxliadpfkybawpo5u.streamlit.app/';
-}
